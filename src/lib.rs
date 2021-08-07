11f8fee99a2f57a1845b662abb36ca33e86f5a59
André Pesci Cazetto <cazetto.andre@gmail.com>
2021-08-07 06:37:39 -0300

    feat: create and export an API to access the number of cpus in Rust from NodeJS

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 use neon::prelude::*;
 
-fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
-    Ok(cx.string("hello node"))
+fn get_num_cpus(mut cx: FunctionContext) -> JsResult<JsNumber> {
+    Ok(cx.number(num_cpus::get() as f64))
 }
 
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
-    cx.export_function("hello", hello)?;
+    cx.export_function("get", get_num_cpus)?;
     Ok(())
 }
